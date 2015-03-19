0a6026725d97bbe0357ed1e30ee080053c9b9569
dromi <dromi@diku.dk>
2015-03-19 16:59:40 +0100

    Controller integrated in program

--- Controller.py
+++ Controller.py
@@ -1,9 +1,155 @@
+import re
+import random
+
 import ASCIIPrinter
+import Board
+
+max_height = 30
+max_width = 26
+
+alphabet = "abcdefghijklmnopqrstuvwxyz"
 
 class Controller(object):
     """Controller class for processing user inputs"""
 
-    def __init__(self, ASCIIPrint):
+    def __init__(self, ap):
         """initialize controller"""
-        self.printer = ASCIIPrint
+        self.view = ap
+        self.field = None
+        self.height = None
+        self.width = None
+        self.mine_no = None
+        self.game_status = "not_in_game"
 
+    def setGameMode(self):
+        """Set parameters for game mode"""
+        while(True):
+            try:
+                self.view.setMsg("Choose difficulty level:")
+                self.view.pushToBody("1: Easy")
+                self.view.pushToBody("2: Medium")
+                self.view.pushToBody("3: Hard")
+                self.view.pushToBody("4: Custom")
+                self.view.flush()
+                inp = self.view.getUserInput()
+                i = int(inp)
+                if i == 1:
+                    self.height  = 10
+                    self.width   = 10
+                    self.mine_no = 10
+                    break
+                elif i == 2:
+                    self.height  = 15
+                    self.width   = 15
+                    self.mine_no = 15
+                    break
+                elif i == 3:
+                    self.height  = 20
+                    self.width   = 20
+                    self.mine_no = 20
+                    break
+                elif i == 4:
+                    self.getCustomParams()
+                    break
+                else:
+                    raise ValueError
+            except ValueError:
+                self.view.print("Invalid input!")
+                self.view.getUserConfirm()
+
+    def getCustomParams(self):
+        """Set custom parameters for game mode"""
+        self.view.setMsg("Input game parameters")
+
+        while(not self.height):
+            try:
+                self.view.pushToBody("Height:")
+                self.view.flush()
+                inp = self.view.getUserInput()
+                i = int(inp)
+                if (0 < i < max_height):
+                    self.height = int(inp)
+                else:
+                    self.view.pushToBody("ERROR: Height must be between 1 and " + str(max_height) + "\n")
+            except ValueError:
+                self.view.pushToBody("ERROR: Could not convert data to an integer\n")
+        
+        while(not self.width):
+            try:
+                self.view.pushToBody("Width:")
+                self.view.flush()
+                inp = self.view.getUserInput()
+                i = int(inp)
+                if (0 < i < max_width):
+                    self.width = int(inp)
+                else:
+                    self.view.pushToBody("ERROR: Width must be between 1 and " + str(max_width) + "\n")
+            except ValueError:
+                self.view.pushToBody("ERROR: Could not convert data to an integer\n")
+
+        while(not self.mine_no):
+            try:
+                self.view.pushToBody("Number of mines:")
+                self.view.flush()
+                inp = self.view.getUserInput()
+                i = int(inp)
+                if (0 < i < (self.height*self.width-9)):
+                    self.mine_no = int(inp)
+                else:
+                    self.view.pushToBody("ERROR: Number of mines must be between 1 and " + str(self.height*self.width-9) + "\n")
+            except ValueError:
+                self.view.pushToBody("ERROR: Could not convert data to an integer\n")
+
+
+    def run(self):
+        """Run the main game loop"""
+        self.field = Board.Board(self.height, self.width, self.mine_no)
+
+        self.game_status = "first_move"
+        self.view.setMsg("Make your first move:")
+        while self.game_status in ["running","first_move"]:
+            self.view.print(self.field.getBoard())
+            try:
+                inp = self.view.getUserInput()
+                chars = re.findall("[a-zA-Z]+", inp)
+                nums = re.findall("[0-9]+", inp)
+                if chars[0].lower() in ["exit","quit"]:
+                    self.game_status = "quitting"
+                elif chars[0].lower() == "flag":
+                    if self.game_status == "first_move":
+                        raise ValueError("Can't flag on first turn")
+                    if (len(nums) == 1 and len(chars) == 2 and chars[1] in alphabet[:self.width] and int(nums[0]) in range(self.height)):
+                        x = alphabet.index(chars[1])
+                        y = int(nums[0])
+                        self.field.flagTile(x,y)
+                elif ((len(nums) == len(chars) == 1) and chars[0] in alphabet[:self.width] and int(nums[0]) in range(self.height)):
+                    x = alphabet.index(chars[0])
+                    y = int(nums[0])
+                    if self.game_status == "first_move":
+                        self.field.genBoard(x,y)
+                    self.game_status = self.field.turnTile(x,y)
+                else:
+                    raise ValueError("Invalid or no input")
+            except ValueError as e:
+                self.view.setMsg("ValueError occured: " + str(e))
+            except IndexError as e:
+                self.view.setMsg("IndexError occured: " + str(e))
+            except IOError as e:
+                self.view.setMsg("IOError occured: " + str(e))
+
+        if(self.game_status == "won"):
+            self.view.setMsg("Game won, gratz!!")
+        elif(self.game_status == "bombed"):
+            self.view.setMsg(random.choice(["OOOhh you got served bro!",
+                                            "You are LITTERALY the bomb! Well done!",
+                                            "You blew up.. does that make you feel any better about yourself?",
+                                            "You got your silly arse bombed"]))
+        elif(self.game_status == "quitting"):
+            self.view.setMsg("Quitting game.. you quitter!")
+            if self.field.empty:
+                self.field.genBoard(random.randint(0,self.width-1), random.randint(0,self.height-1))
+        else:
+            self.view.setMsg("unexpected game endning:", self.game_status)
+        self.field.flipBoard()
+        self.view.print(self.field.getBoard())
+        self.view.getUserConfirm()
--- Pysweeper.py
+++ Pysweeper.py
@@ -8,6 +8,7 @@ import platform
 
 import Board
 import ASCIIPrinter
+import Controller
 
 max_height = 30
 max_width = 26
@@ -15,8 +16,8 @@ max_width = 26
 alphabet = "abcdefghijklmnopqrstuvwxyz"
 
 def help():
-    p.setMsg("Help:")
-    p.print("""\
+    view.setMsg("Help:")
+    view.print("""\
 Pysweeper. Like Minesweeper you know?..
 
   - : Empty spot with no adjecent mines
@@ -26,179 +27,55 @@ Pysweeper. Like Minesweeper you know?..
   * : Mine
   X : Exploded mine\
 """)
-    p.getUserConfirm()
+    view.getUserConfirm()
 
 def options():
-    p.setMsg("Options:")
-    p.pushToBody("Graphical settings:\n")
-    p.pushToBody("1: ASCII              <---")
-    p.pushToBody("2: Fancy Fancy GUI ")
-    p.flush()
-    inp = p.getUserInput()
+    view.setMsg("Options:")
+    view.pushToBody("Graphical settings:\n")
+    view.pushToBody("1: ASCII              <---")
+    view.pushToBody("2: Fancy Fancy GUI")
+    view.flush()
+    inp = view.getUserInput()
     if inp == "1":
-        p.print("Good choice!\n")
-        p.getUserConfirm()
+        view.print("Good choice!\n")
+        view.getUserConfirm()
     elif inp == "2":
-        p.print("ERROR Only ASCII graphics avaliable (this is after all an ASCII game..)\n")
-        p.getUserConfirm()
+        view.print("ERROR Only ASCII graphics avaliable (this is after all an ASCII game..)\n")
+        view.getUserConfirm()
     else:
-        p.print("ERROR! Bad input. No optioning for you\n")
-        p.getUserConfirm()
-
-def getParams():
-    p.setMsg("Input game parameters")
-
-    height = None
-    width = None
-    mine_no = None
-
-    while(height == None):
-        try:
-            p.pushToBody("Height:")
-            p.flush()
-            inp = p.getUserInput()
-            i = int(inp)
-            if (0 < i < max_height):
-                height = int(inp)
-            else:
-                p.pushToBody("ERROR: Height must be between 1 and " + str(max_height) + "\n")
-        except ValueError:
-            p.pushToBody("ERROR: Could not convert data to an integer\n")
-
-    while(width == None):
-        try:
-            p.pushToBody("Width:")
-            p.flush()
-            inp = p.getUserInput()
-            i = int(inp)
-            if (0 < i < max_width):
-                width = int(inp)
-            else:
-                p.pushToBody("ERROR: Width must be between 1 and " + str(max_width) + "\n")
-        except ValueError:
-            p.pushToBody("ERROR: Could not convert data to an integer\n")
-
-    while(mine_no == None):
-        try:
-            p.pushToBody("Number of mines:")
-            p.flush()
-            inp = p.getUserInput()
-            i = int(inp)
-            if (0 < i < (height*width-9)):
-                mine_no = int(inp)
-            else:
-                p.pushToBody("ERROR: Number of mines must be between 1 and " + str(height*width-9) + "\n")
-        except ValueError:
-            p.pushToBody("ERROR: Could not convert data to an integer\n")
-
-    return [height, width, mine_no]
-
-def run():
-    """Run the main game loop"""
-
-    # Get input for size and no of mines
-    [height, width, mine_no] = getParams()
-
-    """
-    8 lines title
-    3 lines spacing and info
-    1 line letters
-    2 lines pr row
-    1 row final
-    2 lines user input
-    ==
-    14 + 2*rows
-    """
-    # os.system("mode con lines=" + str(18+2*height)) #resize cmd window to fit field
-
-    """
-    3 numbers
-    4 pr col
-    1 final
-    4 for symetry
-    ==
-    4 + 4*cols
-    """
-
-    # if width > 18:
-    #     os.system("mode con cols=" + str(7+4*width)) #resize cmd window to fit field
-    # else:
-    #     os.system("mode con cols=80") #resize cmd window to default size
-
-    field = Board.Board(height, width, mine_no)
-
-    game_status = "first_move"
-    p.setMsg("Make your first move:")
-    while game_status in ["running","first_move"]:
-        p.print(field.getBoard())
-        try:
-            inp = p.getUserInput()
-            chars = re.findall("[a-zA-Z]+", inp)
-            nums = re.findall("[0-9]+", inp)
-            if chars[0].lower() in ["exit","quit"]:
-                game_status = "quitting"
-            elif chars[0].lower() == "flag":
-               if game_status == "first_move":
-                    raise IOError("Can't flag on first turn")
-               if (len(nums) == 1 and len(chars) == 2 and chars[1] in alphabet[:width] and int(nums[0]) in range(height)):
-                    x = alphabet.index(chars[1])
-                    y = int(nums[0])
-                    field.flagTile(x,y)
-            elif ((len(nums) == len(chars) == 1) and chars[0] in alphabet[:width] and int(nums[0]) in range(height)):
-                x = alphabet.index(chars[0])
-                y = int(nums[0])
-                if game_status == "first_move":
-                    field.genBoard(x,y)
-                game_status = field.turnTile(x,y)
-            else:
-                 raise ValueError("Invalid or no input")
-        except ValueError as e:
-            p.setMsg("ValueError occured: " + str(e))
-        except IndexError as e:
-            p.setMsg("IndexError occured: " + str(e))
-        except IOError as e:
-            p.setMsg("IOError occured: " + str(e))
-
-    if(game_status == "won"):
-        p.setMsg("Game won, gratz!!")
-    elif(game_status == "bombed"):
-        p.setMsg(random.choice(["OOOhh you got served bro!",
-                                "You are LITTERALY the bomb! Well done!",
-                                "You blew up.. does that make you feel any better about yourself?",
-                                "You got your silly arse bombed"]))
-    elif(game_status == "quitting"):
-        p.setMsg("Quitting game.. you quitter!")
-        if field.empty:
-            field.genBoard(random.randint(0,width-1), random.randint(0,height-1))
-    else:
-        p.setMsg("unexpected game endning:", game_status)
-    field.flipBoard()
-    p.print(field.getBoard())
-    p.getUserConfirm()
+        view.print("ERROR! Bad input. No optioning for you\n")
+        view.getUserConfirm()
 
 def main_menu():
     while(True):
-        p.setMsg("Please make your selection:")
-        p.pushToBody("1: New Game")
-        p.pushToBody("2: Options")
-        p.pushToBody("3: Help")
-        p.pushToBody("4: Quit")
-        p.flush()
-        inp = p.getUserInput()
-        if inp == "1":
-            run()
-        elif inp == "2":
-            options()
-        elif inp == "3":
-            help()
-        elif inp == "4":
-            p.print("Exiting..")
-            return
-        else:
-            p.print("Invalid input!")
-            p.getUserConfirm()
+        try:
+            view.setMsg("Please make your selection:")
+            view.pushToBody("1: New Game")
+            view.pushToBody("2: Options")
+            view.pushToBody("3: Help")
+            view.pushToBody("4: Quit")
+            view.flush()
+            inp = view.getUserInput()
+            i = int(inp)
+            if i == 1:
+                controller.setGameMode()
+                controller.run()
+            elif i == 2:
+                options()
+            elif i == 3:
+                help()
+            elif i == 4:
+                view.print("Exiting..")
+                return
+            else:
+                view.print("Invalid input!")
+                view.getUserConfirm()
+        except ValueError:
+                view.print("Not a damn number")
+                view.getUserConfirm()
 
 if __name__ == '__main__':
     #os.system("mode con lines=30") #resize cmd window
-    p = ASCIIPrinter.ASCIIPrinter()
+    view = ASCIIPrinter.ASCIIPrinter()
+    controller = Controller.Controller(view)
     main_menu()
